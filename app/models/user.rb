e9d5599a889854f1b36fada5c77944c3b8c3383d
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-08-30 03:12:34 +0000

    [add]本の投稿ができるように。プロフィールを編集できるように。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -12,6 +12,9 @@ class User < ApplicationRecord
 
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    unless profile_image.attached?
+      profile_image : 'no_image.jpg'
+    else
+      
   end
 end
